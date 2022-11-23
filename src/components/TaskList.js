9b20d5c82fde846afe36c2a617663f70d9e4bbc3
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-23 11:14:57 -0600

    implement deletion using callbacks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from './Task';
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList(props) {
+  const { todos } = props;
+
+  const nextID = (
+    (id) => () =>
+      id++
+  )(0);
+
+  const myTodos = todos.map((todo) => {
+    return {
+      id: nextID(),
+      text: todo.text,
+      category: todo.category,
+    };
+  });
+
+  const [ todoList, updateTodoList ] = useState(myTodos);
+
+  const deleteTodo = (id) => {
+    const newTodos = todoList.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    updateTodoList(newTodos);
+  };
+
+  const makeList = () => {
+    return todoList.map((todo) => {
+      return <Task key={todo.id} callBack={deleteTodo} task={todo} />;
+    });
+  };
+
+  return <div className="tasks">{makeList()}</div>;
 }
 
 export default TaskList;
